66d691e09b3fc8321311cf0cb0fc3d8873e90660
Threekoolkatz <MacKenzie.Smith@mines.sdsmt.edu>
2016-01-29 17:03:39 -0700

    Renamed Header.h to ANNTest.h, added neuron class and tested basic functionality.

--- prog1/ANNTest/main.cpp
+++ prog1/ANNTest/main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include "ANNTest.h"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	cout << "Hello ANNTest! \n ";
+	//cout << "Hello ANNTest! \n ";
+
+	double testNum = 5.21;
+	double testNums[2] = { 5.223, 213.244 };
+
+	neuron test1;
+	neuron test2(testNum);
+	neuron test3(testNums);
+
+	cout << "Here is test1: " << test1.input << ", sum: " << test1.sum << endl;
+	cout << "Here is test2: " << test2.input << ", sum: " << test2.sum << endl;
+	cout << "Here is test3: " << test3.input << ", sum: " << test3.sum << endl;
 	
 	return 0;
 }
