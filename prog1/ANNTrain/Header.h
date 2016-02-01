917b151177362e93e8eaeb81e9c2ec12073ae9c2
BergerKing <marcus.berger@mines.sdsmt.edu>
2016-01-31 19:07:12 -0700

    Added parseCSV code

--- prog1/ANNTrain/Header.h
+++ prog1/ANNTrain/Header.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -10,4 +14,20 @@ class ParameterFileReader
 {
 	public:
 		void ParseParameterFile();
+};
+
+class CSVFileReader
+{
+	public:
+		vector<vector<string>> records;
+		vector<string> headings;
+		string filename;
+
+		// ReadDataFile destroys the passed in vectors content
+		void ReadDataFile(vector<vector<string>> &records, 
+			vector<string> &headings, string filename);
+
+	private:
+		int getRows(string filename);
+
 };
--- prog1/ANNTrain/main.cpp
+++ prog1/ANNTrain/main.cpp
@@ -1,20 +1,37 @@
 #include "Header.h"
+#include <regex>
 
 int main(int argc, char *argv[])
 {
+
+	CSVFileReader csvFileReader;
 	ParameterFileReader paramFileReader;
 
-	if (!CheckArguments(argc))
+	// replace with correct commandline argument (Marcus)------------------------
+	csvFileReader.filename = "PDSI_BH_1978-2015.csv";
+	// -------------------------------------------------------------------------
+	
+	/* commented out for testing purposes (Marcus)
+	if (!CheckArguments(argc,argv[]))
 	{
 		return 0;
 	}
+	*/
 
 	paramFileReader.ParseParameterFile();
+	csvFileReader.ReadDataFile(csvFileReader.records, csvFileReader.headings,
+		csvFileReader.filename);
+	if (csvFileReader.records.empty() == true ||
+		csvFileReader.headings.empty() == true)
+	{
+		cout << "An error occurred while reading csv file, exiting program.";
+		return -1;
+	}
 
 	return 0;
 }
 
-bool CheckArguments(int argc)
+bool CheckArguments(int argc, char *argv[])
 {
 	bool retVal = true;
 
@@ -22,6 +39,8 @@ bool CheckArguments(int argc)
 	{
 		retVal = false;
 	}
+	if (regex_match(argv[1], regex("(.*csv)")) == false)
+		retVal = false;
 
 	return retVal;
 }
